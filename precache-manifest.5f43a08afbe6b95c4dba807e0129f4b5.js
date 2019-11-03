34733ac8abeeaf6fee4ac04d4a9213ec57e6efa0
brendapsd <brenda.sd.15@gmail.com>
2019-11-03 16:17:41 -0500

    Updates

---
+++ precache-manifest.5f43a08afbe6b95c4dba807e0129f4b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfc172c98d152df8ae098f8de63de91",
+    "url": "/clima-app-react/index.html"
+  },
+  {
+    "revision": "b4225ba09b3da1c8d14f",
+    "url": "/clima-app-react/static/css/main.3751ae63.chunk.css"
+  },
+  {
+    "revision": "12f0a4ed4020382340c5",
+    "url": "/clima-app-react/static/js/2.82d17c1c.chunk.js"
+  },
+  {
+    "revision": "b4225ba09b3da1c8d14f",
+    "url": "/clima-app-react/static/js/main.7195a50b.chunk.js"
+  },
+  {
+    "revision": "845a24b4c3c3bb65d9ff",
+    "url": "/clima-app-react/static/js/runtime-main.a6597f3b.js"
+  }
+]);
